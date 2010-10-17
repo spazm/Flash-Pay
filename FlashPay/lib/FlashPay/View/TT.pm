780251f2ff51ae01bc8a43aca79aa37141ede733
Andrew Grangaard <granny-github@ofb.net>
2010-10-17 03:49:04 -0700

    remove template_extension, as per demo

--- FlashPay/lib/FlashPay/View/TT.pm
+++ FlashPay/lib/FlashPay/View/TT.pm
@@ -6,7 +6,7 @@ use warnings;
 use base 'Catalyst::View::TT';
 
 __PACKAGE__->config(
-    TEMPLATE_EXTENSION => '.tt2',
+#    TEMPLATE_EXTENSION => '.tt2',
     render_die         => 1,
     INCLUDE_PATH       => [ FlashPay->path_to( 'root', 'src' ), ],
 
